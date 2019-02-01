37552d869e20d36008c0ae83690638321b8927e6
alpha018 <tas001@alumnos.ucn.cl>
2019-01-31 22:22:03 -0300

    modify: more test

--- app.js
+++ app.js
@@ -209,13 +209,13 @@ app.post('/print', (req, res) => {
 
             for (let i = 0; i < params.prendas.length; i++) {
                 printer.tableCustom([
-                    {text: params.prendas[i].cant, align: "LEFT", width: 0.2},
+                    {text: params.prendas[i].cant, align: "LEFT", width: 0.3},
                     {text: params.prendas[i].prenda.toUpperCase(), align: "CENTER", width: 0.3},
                     {text: params.prendas[i].valor, align: "RIGHT", width: 0.3}
                 ]);
             }
             printer.tableCustom([
-                {text: 'TOTAL', align: "CENTER", width: 0.3, bold: true},
+                {text: 'TOTAL', align: "CENTER", width: 0.5, bold: true},
                 {text: params.total, align: "RIGHT", width: 0.5}
             ]);
 
@@ -290,7 +290,7 @@ app.post('/print', (req, res) => {
                 printer.leftRight('DESPACHO DOMICILIO', text);
                 printer.drawLine();
                 printer.tableCustom([
-                    {text: "CANT", align: "LEFT", width: 0.2, bold: true},
+                    {text: "CANT", align: "LEFT", width: 0.3, bold: true},
                     {text: "PRENDA", align: "CENTER", width: 0.3, bold: true},
                     {text: "VALOR", align: "RIGHT", width: 0.3, bold: true}
                 ]);
